148697bd0594eaff84baeab9b340b92456f8a1a0
Mickeyzil <mikizil04@gmail.com>
2026-01-15 11:24:28 +0200

    main file

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>  
+#include <ctime>    
+#include "Game.hpp"
+#include "HumanPlayer.hpp"
+#include "AiPlayer.hpp"
+
+int main()
+{
+    std::srand(std::time(nullptr));
+    HumanPlayer* player1 = new HumanPlayer("Human");
+    AiPlayer* player2 = new AiPlayer();
+
+    Game game(player1, player2);
+    game.setup();    
+    game.start();
+       
+    delete player1;
+    delete player2;
+     return 0;
+}
